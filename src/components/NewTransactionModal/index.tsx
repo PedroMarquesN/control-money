14141bef095ed216bb2c12973164e537f6b96f5f
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-03-21 13:38:05 -0300

    Feat: finish modal css

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import {CloseButton, Content, Overlay} from "./styles.ts";
-import {X} from "phosphor-react";
+import {CloseButton, Content, Overlay, TransactionType, TransactionTypeButton} from "./styles.ts";
+import {ArrowCircleDown, ArrowCircleUp, X} from "phosphor-react";
 
 export function NewTransactionModal() {
     return(
@@ -15,6 +15,17 @@ export function NewTransactionModal() {
                     <input type="text" placeholder="Descrição"/>
                     <input type="number" placeholder="Preço"/>
                     <input type="text" placeholder="Categoria"/>
+                    <TransactionType>
+                        <TransactionTypeButton value='income' variant='income'>
+                            <ArrowCircleUp size={24}/>
+                            Entrada
+                        </TransactionTypeButton>
+                        <TransactionTypeButton value='outcome' variant='outcome'>
+                            <ArrowCircleDown size={24}/>
+                            Saída
+                        </TransactionTypeButton>
+                    </TransactionType>
+
                     <button type='submit'>Cadastrar</button>
                 </form>
 
