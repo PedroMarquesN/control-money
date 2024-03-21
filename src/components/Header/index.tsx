aac1a18929de072458ad44c2bd3a497300fb890d
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-03-21 13:04:56 -0300

    Feat: Create modal component and styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import {HeaderContainer, HeaderContent, NewTransactionButton} from "./styles.ts"
 //import IconeIgnite from '../../assets/logo.svg'
 import ImageLogo from '../../assets/_28fe16db-da61-4173-a698-358407438915.jpeg'
 import * as Dialog from '@radix-ui/react-dialog';
+import {NewTransactionModal} from "../NewTransactionModal";
 export function Header() {
     return (
         <HeaderContainer>
@@ -11,13 +12,7 @@ export function Header() {
                     <Dialog.Trigger asChild>
                         <NewTransactionButton>Nova Transação</NewTransactionButton>
                     </Dialog.Trigger>
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
-                        <Dialog.Content>
-                            <Dialog.Title>Nova Transação</Dialog.Title>
-                            <Dialog.Close />
-                        </Dialog.Content>
-                    </Dialog.Portal>
+                    <NewTransactionModal/>
                 </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
